a21677fcb25dd19225aac5f3982021c0e449dad3
Guillermo Andrada <guillermoandrada@gmail.com>
2021-07-06 10:49:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Guillermo-CV-Henry
+
+## Repositorio para Henry
+
+---
+
+Trabajo práctico para Henry.
+
+Solicitaron entre otras cosas, lo siguiente:
+
+## Ejercicio II
+
+```
+Usando lo que aprendiste de HTML vas a crear un nuevo documento dentro de la carpeta de 'homework' llamado 'cv.html'. En el mismo vas crear tu propio Curriculum, deberá tener las siguientes secciones:
+
+* Titulo
+* Descripción Personal
+* Educación
+* Experiencias Laborales
+* Skills
+* Tu foto
+* Hobbies
+
+Además agregá una sección más donde expliques por qué estás estudiando esta carrera y cuáles son tus expectativas al finalizar la carrera.
+
+Subir también este archivo a su repositorio de github
+
+```
+
+> Para este ejercicio vos vas a tener que elegir que **tags** html usar para darle la mejor estructuración al html. Acordate que en el futuro vamos a ver más en profundidad los estilos CSS, por lo que por ahora no te preocupes tanto del layout más allá de las reglas básicas.
+
+---
+
+Yo decidí hacerlo en Inglés, y de paso practicar un poco.
---
+++ script.js
@@ -0,0 +1,82 @@
+document.getElementById("itemHome").onclick = function () { mostrarHome() };
+document.getElementById("itemAbout").onclick = function () { mostrarAbout() };
+document.getElementById("itemEducation").onclick = function () { mostrarEducation() };
+document.getElementById("itemExperience").onclick = function () { mostrarExperience() };
+document.getElementById("itemSkills").onclick = function () { mostrarSkills() };
+document.getElementById("itemHobbies").onclick = function () { mostrarHobbies() };
+document.getElementById("itemMore").onclick = function () { mostrarMore() };
+
+/* Lista de funciones */
+function mostrarHome() {
+    document.getElementById("homeView").classList.replace("invisible", "visible");
+    document.getElementById("aboutView").classList.replace("visible", "invisible");
+    document.getElementById("educationView").classList.replace("visible", "invisible");
+    document.getElementById("experienceView").classList.replace("visible", "invisible");
+    document.getElementById("skillsView").classList.replace("visible", "invisible");
+    document.getElementById("hobbiesView").classList.replace("visible", "invisible");
+    document.getElementById("moreView").classList.replace("visible", "invisible");  
+}
+
+function mostrarAbout() {
+    document.getElementById("homeView").classList.replace("visible", "invisible");
+    document.getElementById("aboutView").classList.replace("invisible", "visible");
+    document.getElementById("educationView").classList.replace("visible", "invisible");
+    document.getElementById("experienceView").classList.replace("visible", "invisible");
+    document.getElementById("skillsView").classList.replace("visible", "invisible");
+    document.getElementById("hobbiesView").classList.replace("visible", "invisible");
+    document.getElementById("moreView").classList.replace("visible", "invisible"); 
+}
+
+function mostrarEducation() {
+    document.getElementById("homeView").classList.replace("visible", "invisible");
+    document.getElementById("aboutView").classList.replace("visible", "invisible");
+    document.getElementById("educationView").classList.replace("invisible", "visible");
+    document.getElementById("experienceView").classList.replace("visible", "invisible");
+    document.getElementById("skillsView").classList.replace("visible", "invisible");
+    document.getElementById("hobbiesView").classList.replace("visible", "invisible");
+    document.getElementById("moreView").classList.replace("visible", "invisible"); 
+}
+
+function mostrarExperience() {
+    document.getElementById("homeView").classList.replace("visible", "invisible");
+    document.getElementById("aboutView").classList.replace("visible", "invisible");
+    document.getElementById("educationView").classList.replace("visible", "invisible");
+    document.getElementById("experienceView").classList.replace("invisible", "visible");
+    document.getElementById("skillsView").classList.replace("visible", "invisible");
+    document.getElementById("hobbiesView").classList.replace("visible", "invisible");
+    document.getElementById("moreView").classList.replace("visible", "invisible"); 
+}
+
+function mostrarSkills() {
+    document.getElementById("homeView").classList.replace("visible", "invisible");
+    document.getElementById("aboutView").classList.replace("visible", "invisible");
+    document.getElementById("educationView").classList.replace("visible", "invisible");
+    document.getElementById("experienceView").classList.replace("visible", "invisible");
+    document.getElementById("skillsView").classList.replace("invisible", "visible");
+    document.getElementById("hobbiesView").classList.replace("visible", "invisible");
+    document.getElementById("moreView").classList.replace("visible", "invisible"); 
+}
+
+function mostrarHobbies() {
+    document.getElementById("homeView").classList.replace("visible", "invisible");
+    document.getElementById("aboutView").classList.replace("visible", "invisible");
+    document.getElementById("educationView").classList.replace("visible", "invisible");
+    document.getElementById("experienceView").classList.replace("visible", "invisible");
+    document.getElementById("skillsView").classList.replace("visible", "invisible");
+    document.getElementById("hobbiesView").classList.replace("invisible", "visible");
+    document.getElementById("moreView").classList.replace("visible", "invisible"); 
+}
+
+function mostrarMore() {
+    document.getElementById("homeView").classList.replace("visible", "invisible");
+    document.getElementById("aboutView").classList.replace("visible", "invisible");
+    document.getElementById("educationView").classList.replace("visible", "invisible");
+    document.getElementById("experienceView").classList.replace("visible", "invisible");
+    document.getElementById("skillsView").classList.replace("visible", "invisible");
+    document.getElementById("hobbiesView").classList.replace("visible", "invisible");
+    document.getElementById("moreView").classList.replace("invisible", "visible"); 
+}
+
+/* Debe haber alguna forma inteliente de
+ modificar la class de todos los selectores a la vez, 
+ pero no la sé... */
